6a5628b2680f20914bed6995533117f27608de09
sumbyKB <506179965@qq.com>
2018-10-23 11:45:41 +0800

    预览图片代码上传

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,9 +1,16 @@
 <!--  -->
 <template>
-  <div>订单实例模板</div>
+    <van-button @click="showImagePreview">{{  }}</van-button>
 </template>
 
 <script>
+import { ImagePreview } from 'vant'
+const images = [
+  'https://img.yzcdn.cn/public_files/2017/09/05/3bd347e44233a868c99cf0fe560232be.jpg',
+  'https://img.yzcdn.cn/public_files/2017/09/05/c0dab461920687911536621b345a0bc9.jpg',
+  'https://img.yzcdn.cn/public_files/2017/09/05/4e3ea0898b1c2c416eec8c11c5360833.jpg',
+  'https://img.yzcdn.cn/public_files/2017/09/05/fd08f07665ed67d50e11b32a21ce0682.jpg'
+]
 export default {
   data () {
     return {
@@ -16,7 +23,19 @@ export default {
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    showImagePreview(position, timer) {
+        const instance = ImagePreview({
+          images,
+          startPosition: typeof position === 'number' ? position : 0
+        });
+        if (timer) {
+          setTimeout(() => {
+            instance.close();
+          }, timer);
+        }
+      }
+  }
 }
 
 </script>
